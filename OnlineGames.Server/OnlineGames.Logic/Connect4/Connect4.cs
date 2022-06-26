285cecf2303a25a50c73e30f644d5d31e5c95503
SashoVas <sando.vasilchev@gmail.com>
2022-06-26 20:19:21 +0300

    Adding move priority in connect4 game

--- OnlineGames.Server/OnlineGames.Logic/Connect4/Connect4.cs
+++ OnlineGames.Server/OnlineGames.Logic/Connect4/Connect4.cs
@@ -21,6 +21,7 @@ namespace OnlineGames.Logic.Connect4
             {
                 return Solver[boardId].Score;
             }
+            //if col is 3 score the value of the move more
             var score = oldScore + board.EvaluateBoard(oldRow, oldCol) + (oldCol == 3 ? player * 6 : 0);
             if (turnCount >= Depth)
             {
@@ -44,28 +45,28 @@ namespace OnlineGames.Logic.Connect4
             {
                 Score = player == 1 ? -999999999 : 999999999
             };
-
-            foreach (var i in TurnsOrder)
+            var newTurnOrder = GetOrderArr(board,player);
+            foreach (var element in newTurnOrder)
             {
-
-                int newY = board.MakeAMove(player, i);
+                int move = element.move;
+                int newY = board.MakeAMove(player, move);
                 if (newY != -1)
                 {
                     //The move is posible
-                    var outcome = FillSolver(board, -player, turnCount + 1, alpha, beta, score, newY, i);
+                    var outcome = FillSolver(board, -player, turnCount + 1, alpha, beta, score, newY, move);
                     if (player == 1)
                     {
                         //First player
                         if (outcome > Solver[boardId].Score)
                         {
-                            Solver[boardId].Col = i;
+                            Solver[boardId].Col = move;
                             Solver[boardId].Score = outcome;
                         }
                         alpha = Math.Max(alpha, outcome);
                         if (alpha >= beta)
                         {
                             //Pruning
-                            board.UndoMove(newY, i,player);
+                            board.UndoMove(newY, move,player);
                             Solver.Remove(boardId);
                             return 1000;
                         }
@@ -75,22 +76,66 @@ namespace OnlineGames.Logic.Connect4
                         //Second player
                         if (outcome < Solver[boardId].Score)
                         {
-                            Solver[boardId].Col = i;
+                            Solver[boardId].Col = move;
                             Solver[boardId].Score = outcome;
                         }
                         beta = Math.Min(beta, outcome);
                         if (alpha >= beta)
                         {
                             //Pruning
-                            board.UndoMove(newY, i,player);
+                            board.UndoMove(newY, move,player);
                             Solver.Remove(boardId);
                             return -1000;
                         }
                     }
-                    board.UndoMove(newY, i,player);
+                    board.UndoMove(newY, move,player);
                 }
             }
             return Solver[boardId].Score;
+        }
+        private (int score, int move)[] GetOrderArr(Board board, int player)
+        {
+            (int score, int move)[] order = new (int score, int move)[7];
+            for (int currnet = 0; currnet < TurnsOrder.Count(); currnet++)
+            {
+                var y = board.MakeAMove(player, TurnsOrder[currnet]);
+                if (y != -1)
+                {
+                    var score = board.EvaluateBoard(y, TurnsOrder[currnet]);
+                    score += TurnsOrder[currnet] == 3 ? player * 6 : 0;
+                    AddToList(order, player, score, TurnsOrder[currnet], currnet);
+                    board.Win = false;
+                    board.Lose = false;
+                    board.UndoMove(y, TurnsOrder[currnet], player);
+                }
+            }
+            return order;
+        }
+        private void AddToList((int score, int move)[] arr, int player, int score, int move, int n)
+        {
+            if (n == 0)
+            {
+                arr[n] = (score, move);
+                return;
+            }
+            if (player == 1)
+            {
+                while (n > 0 && arr[n - 1].score < score)
+                {
+                    arr[n] = arr[n - 1];
+                    n--;
+                }
+                arr[n] = (score, move);
+            }
+            else
+            {
+                while (n > 0 && arr[n - 1].score > score)
+                {
+                    arr[n] = arr[n - 1];
+                    n--;
+                }
+                arr[n] = (score, move);
+            }
         }
     }
 }
--- OnlineGames.Server/OnlineGames.Logic/Connect4/Helper/CellCoordinates.cs
+++ OnlineGames.Server/OnlineGames.Logic/Connect4/Helper/CellCoordinates.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlineGames.Logic.Connect4.Helper
+﻿namespace OnlineGames.Logic.Connect4.Helper
 {
     public class CellCoordinates
     {
