69ec6a0f945f3f0dda6c8f22ee00a92c0df5148a
SashoVas <98760930+SashoVas@users.noreply.github.com>
2024-10-24 22:15:07 +0300

    Adding tests

---
+++ OnlineGames.Server/OnlineGames.Tests/Infrasturcture/OnlineGamesDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineGames.Data;
+using System;
+
+
+namespace OnlineGames.Tests.Infrasturcture
+{
+    internal class OnlineGamesDbContextFactory
+    {
+        public static OnlineGamesDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<OnlineGamesDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            
+            var context = new OnlineGamesDbContext(options);
+            return context;
+        }
+    }
+}
---
+++ OnlineGames.Server/OnlineGames.Tests/Services/MessageServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using OnlineGames.Data;
+using OnlineGames.Data.Models;
+using OnlineGames.Services;
+using OnlineGames.Services.Contracts;
+using OnlineGames.Tests.Infrasturcture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineGames.Tests.Services
+{
+    public class MessageServiceTests
+    {
+        private List<Message> GetDummyData()
+        {
+            var messages = new List<Message>
+            {
+                new Message { Id = 1, Contents = "Unread message", PostedOn = DateTime.Now, Seen = false, SenderId = "user2", FriendChat = new Friend { Id="id1", User1Id = "user1", User2Id = "user2" } },
+                new Message { Id = 2, Contents = "Read message", PostedOn = DateTime.Now.AddMinutes(-1), Seen = true, SenderId = "user1", FriendChat = new Friend { Id="id2", User1Id = "user1", User2Id = "user2" } }
+            };
+            return messages;
+        }
+        private async Task SeedData(OnlineGamesDbContext context)
+        {
+            context.Messages.AddRange(GetDummyData());
+            await context.SaveChangesAsync();
+
+        }
+
+        [Fact]
+        public async Task GetMessages_ReturnsCorrectMessages()
+        {
+            // Arrange
+            var context = OnlineGamesDbContextFactory.InitializeContext();
+            await SeedData(context);
+            var repo = new Repository<Message>(context); 
+            var messageService = new MessageService(repo);
+
+            // Act
+            var result = await messageService.GetMessages("user1", "user2", 0);
+
+            // Assert
+            Assert.Equal(2, result.Count());
+            Assert.Equal("Unread message", result.First().Contents);
+        }
+    }
+}
